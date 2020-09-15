3cfe14843b8ba1bba05fc501f308b73f41bb4bc5
IREZON <elvisrugamba@gmail.com>
2020-09-15 11:59:27 +0200

    Fix related articles

--- server/models/Article.js
+++ server/models/Article.js
@@ -115,8 +115,8 @@ const getRelatedArticle = async (newsId, language) => {
     if (rows.length < 0) return null;
 
     const results = await db.query(
-      `SELECT * FROM news WHERE category=$1 AND language=$2 AND status=$3 ORDER BY news_id DESC LIMIT 3`,
-      [rows[0].category, language, 'edited']
+      `SELECT * FROM news WHERE category=$1 AND language=$2 AND status=$3 AND news_id!=$4 ORDER BY news_id DESC LIMIT 3`,
+      [rows[0].category, language, 'edited', newsId]
     );
     return results.rows;
   } catch (error) {
